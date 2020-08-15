3589cd72fadba51ebaac54890cac0cb784ef7afa
Mila <camilasouza@apples-MacBook-Pro.local>
2020-08-15 10:02:59 -0300

    quase todo o desafio

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,66 +1,119 @@
 import React from 'react';
-import './App.css';
 import styled from 'styled-components';
 
-const ContainerComentario = styled.section`
-
+const ContainerApp = styled.section`
+  display: grid;
+  grid-template-columns: 200px 1fr 60px;
+  grid-template-rows: 1fr 25px;
+  grid-column-gap: 10px;
+  padding: 10px;
+  height: 95vh;
+  width: 95vw;
+  background-color: lightpink;
+  border: thin solid black;
+`
+const ContainerNome = styled.span`
+  grid-column: 1 / 2;
+  grid-row: 2 /3;
+` 
+const ContainerComentario = styled.span`
+  grid-column: 2 / 3;
+  grid-row: 2 / 3;
+`
+const BotaoEnviar = styled.button`
+  grid-column: 3 / 4;
+  grid-row: 2 / 3;
+  width: 60px;
+`
+const BoxApp = styled.section`
+  grid-column: 1 / 4;
+  grid-row: 1 / 2;
+  align-self: end;
+  background-color: white;
+  margin: 10px;
+  border-radius: 5px;
+  padding: 5px;
+`
+const BoxPessoa = styled.section`
+  grid-column: 1 / 2;
+  font-weight: bold;
+`
+const BoxComentario = styled.section`
+  grid-column: 1 / 4;
+  margin-bottom: 5px;
+  border-bottom: 1px solid pink;
+`
+const Input = styled.input`
+  width: 99%;
+  height: 20px;
+  border-radius: 5px;
+  align-self: end;
 `
 
-export class App extends React.Component{
+class App extends React.Component{
   state = {
     usuario:[
       {
-        nome:[],
-        comentario:[],
+        nome:"",
+        comentario:"",
       }
     ],
 
     valorInputNome:"",
-    valorInputComentario:""
+    valorInputComentario:"",
   }
+
+  adicionarComentario = () => {
+    const novaPessoa = {
+      nome: this.state.valorInputNome,
+      comentario: this.state.valorInputComentario
+    };
+    const adicionarComentario = [...this.state.usuario,novaPessoa];
+    this.setState({
+      usuario: adicionarComentario,
+      valorInputNome:"",
+      valorInputComentario:"",
+    });
+  } 
 
   onChangeNome = (event) => {
     this.setState({valorInputNome: event.target.value})
-  }
+  };
 
   onChangeComentario = (event) => {
     this.setState({valorInputComentario: event.target.value})
-  }
-
-  aoEnviarMensagem = (event) => {
-      this.setState({nome: event.target.value}, 
-          {comentario: event.target.value})
-    }
-  
-
-  adicionarComentario =(mensagem)=>{
-    this.setState({comentario:[...this.state.comentario,mensagem.value]})
-  }
+  };
 
 
   render() {
-    const listaDeComentarios= this.state.usuario.map((pessoa)=>{
+    const listaDeComentarios = this.state.usuario.map((pessoa) => {
       return(
         <div>
-          <input
-            value={this.state.valorInputNome}
-            onChange={this.onChangeInputNome}
-            placeholder={"Nome"}
-          />
-
-          <input
-            value={this.state.valorInputComentario}
-            onChange={this.onChangeInputComentario}
-            placeholder={"Mensagem"}
-           />
-          <button onClick={this.props.adicionarComentario}>Enviar</button>
-
+          <BoxPessoa>{pessoa.nome}</BoxPessoa>
+          <BoxComentario>{pessoa.comentario}</BoxComentario>
         </div>
        );
-      })
+      });
     return (
-      <div>{listaDeComentarios}</div>
-    )
+      <ContainerApp>
+        <ContainerNome>
+          <Input
+            value = {this.state.valorInputNome}
+            onChange = {this.onChangeNome}
+            placeholder = {"Nome"}
+          />
+        </ContainerNome>
+        <ContainerComentario>
+          <Input
+            value = {this.state.valorInputComentario}
+            onChange = {this.onChangeComentario}
+            placeholder = {"Mensagem"}
+          />
+        </ContainerComentario>
+        <BotaoEnviar onClick  = {this.adicionarComentario}>Enviar</BotaoEnviar>
+    <BoxApp>{listaDeComentarios}</BoxApp>
+    </ContainerApp>
+    );
     
   }
 }
